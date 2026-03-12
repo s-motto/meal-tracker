9d73750c3ff6ae213cc0ed3d7e81c4a3c666543b
s-motto <brinamotto@gmail.com>
2026-03-12 20:41:41 +0100

    feat: add NuovaRicetta route and connect to useRecipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+import { useNavigate } from 'react-router-dom'
+
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import MealLog from './pages/MealLog'
+import RecipeList from './pages/RecipeList'
+import RecipeDetail from './pages/RecipeDetail'
+import RecipeForm from './components/RecipeForm'
+import { useRecipes } from './hooks/useRecipes'
+
+function NuovaRicetta() {
+  const { aggiungi } = useRecipes()
+  const navigate = useNavigate()
+
+  const handleSave = (ricetta) => {
+    aggiungi(ricetta)
+    navigate('/ricette')
+  }
+
+  return <RecipeForm onSave={handleSave} />
+}
+
 export default function App() {
-  return <div>App funzionante</div>
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<MealLog />} />
+        <Route path="/ricette" element={<RecipeList />} />
+        <Route path="/ricette/nuova" element={<NuovaRicetta />} />
+        <Route path="/ricette/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Layout>
+  )
 }
